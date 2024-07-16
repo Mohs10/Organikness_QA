e1badeb6c9e84147464d710821efa0a138cbe182
arkishankumar <ar.kishan.kumar@mohs10.io>
2024-07-16 12:34:51 +0530

    change made payment Status and payment id

---
+++ src/main/java/com/Organics/LyncOrganic/ReuseableCodes/ReusableMethods.java
@@ -0,0 +1,23 @@
+package com.Organics.LyncOrganic.ReuseableCodes;
+
+import org.springframework.stereotype.Service;
+
+
+
+@Service
+public class ReusableMethods {
+
+
+    public boolean isPartialMatch(String source, String target) {
+        if (source.length() < target.length()) {
+            return false;
+        }
+
+        for (int i = 0; i < target.length(); i++) {
+            if (source.charAt(i) != target.charAt(i)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
